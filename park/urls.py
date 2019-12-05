227669002e7be7f7b077e9e3c9106e9a02c6e266
liangSsss <slfoxer7@gmail.com>
2019-12-05 18:50:29 +0000

    Add two paths in urls.py

--- park/urls.py
+++ park/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'park'
 urlpatterns = [
     path('map/',views.map),
+    path('<int:unique_squirrel_id>/',views.squirrel_edit),
+    path('add/',views.squirrel.add),
 ]
