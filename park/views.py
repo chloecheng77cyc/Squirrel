2d0419766b16cc6917dfb196d977313835501af0
liangSsss <slfoxer7@gmail.com>
2019-12-05 18:43:02 +0000

    Write edit/add/delete in views.py

--- park/views.py
+++ park/views.py
@@ -20,6 +20,42 @@ def map(request):
     return render(request, 'park/map.html', context)
 
 
+def add_squirrel(request):
+    if request.method == 'POST':
+        form = SquirrelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/park/sightings/add/')
+    else:
+        form = SquirrelForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'park/edit.html',context)
+
+
+def edit_squirrel(request, unique_squirrel_id):
+    squirrel = Squirrel.objects.get(id=unique-squirrel_id)
+    if request.method == 'POST':
+        form = SquirrelForm(request.POST, instance=squirrel)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/sightings/{unique-squirrel-id}')
+    elif request.method=='DELETE':
+        squirrel.delete()
+        return redirect(f'/sighting_list/{unique-squirrel-id}')
+    else:
+        form = SquirrelForm(instance=squirrel)
+
+    context = {
+        'form':form,
+    }
+
+    return render(request, 'park.edit.html', context)
+
+
 
 
 # Create your views here.
