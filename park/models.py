65cc42b5cefa9256519eb0c437f66049b366895e
liangSsss <slfoxer7@gmail.com>
2019-12-05 18:47:53 +0000

    Write edit.html

--- park/models.py
+++ park/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import gettext as _
 
 
-class Sighting(models.Model):
+class Squirrel(models.Model):
     Latitude = models.FloatField(
         help_text = _('latitude of sighting'),
     )
